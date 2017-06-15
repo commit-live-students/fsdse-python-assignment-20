e25989a310fbd9aa54c4e9735f43b6740c8bd342
sankbhatt <sankbhatt@users.noreply.github.com>
2017-06-15 16:43:16 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,10 @@
+import operator
+
 def solution(dic1):
     '''Enter code here'''
+    return sorted(dic1.items(), key=operator.itemgetter(1))
+
+
+
+a = {1: 10, 2: 20, 3: 30, 4: 40}
+print solution(a)
