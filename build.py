6c7d5d35a7717f2e28c4ce1f647d2fc488e080b2
saahil1292 <saahil1292@gmail.com>
2017-06-19 17:02:34 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,13 @@
 def solution(dic1):
-    '''Enter code here'''
+    list1 = []
+    sorted_list = []
+    for key in dic1:
+        tuples = key, dic1[key]
+        #print tuples
+        list1.append(tuples)
+    #print list1
+    sorted_list = sorted(list1)
+    return sorted_list
+
+dict1 = {1: 10, 2: 20, 3: 30, 4: 40}
+solution(dict1)
