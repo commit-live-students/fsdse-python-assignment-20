c5fbf6974d4439e05a20a1d76aefa3643f03a0c1
rishabh-github <rishabh-github@users.noreply.github.com>
2017-06-23 04:16:03 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,9 @@
 def solution(dic1):
-    '''Enter code here'''
+    list1 = []
+    for key in dic1:
+
+        list1.append((key , dic1[key]))
+
+    return list1
+
+print solution({1: 10, 2: 20, 3: 30, 4: 40})
