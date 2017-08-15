b925d09749478fb2177fd1dc32c5e1c0f08d9a52
rohanhg91 <rohanhgupta91@gmail.com>
2017-08-15 07:16:51 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,16 @@
 def solution(dic1):
     '''Enter code here'''
+    op = ()
+    output = []
+    for i in range (0,len(dic1) - 1):
+        for k, v  in dic1.iteritems():
+            op = (k,v)
+            output.append(op)
+        return output
+
+
+'''
+dic1 = {1: 10, 2: 20, 3: 30, 4: 40}
+a = solution(dic1)
+print a
+'''
